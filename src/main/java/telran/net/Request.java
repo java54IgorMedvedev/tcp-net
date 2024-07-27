6f6677519d0ecf63677d0460475e75b711ecf90e
IgorMedvedev <psydov81@gmail.com>
2024-07-27 18:47:26 +0300

    upd

--- src/main/java/telran/net/Request.java
+++ src/main/java/telran/net/Request.java
@@ -2,13 +2,13 @@ package telran.net;
 
 import org.json.JSONObject;
 import static telran.net.TcpConfigurationProperties.*;
+
 public record Request(String requestType, String requestData) {
-	
-	@Override
-	public String toString() {
-		JSONObject jsonObj = new JSONObject();
-		jsonObj.put(REQUEST_TYPE_FIELD, requestType);
-		jsonObj.put(REQUEST_DATA_FIELD, requestData);
-		return jsonObj.toString();
-	}
+    @Override
+    public String toString() {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put(REQUEST_TYPE_FIELD, requestType);
+        jsonObj.put(REQUEST_DATA_FIELD, requestData);
+        return jsonObj.toString();
+    }
 }
--- src/main/java/telran/net/Response.java
+++ src/main/java/telran/net/Response.java
@@ -2,13 +2,13 @@ package telran.net;
 
 import org.json.JSONObject;
 import static telran.net.TcpConfigurationProperties.*;
-public record Response(ResponseCode responseCode, String ResponseData) {
-	
-	@Override
-	public String toString() {
-		JSONObject jsonObj = new JSONObject();
-		jsonObj.put(RESPONSE_CODE_FIELD, responseCode);
-		jsonObj.put(RESPONSE_DATA_FIELD, ResponseData);
-		return jsonObj.toString();
-	}
+
+public record Response(ResponseCode responseCode, String responseData) {
+    @Override
+    public String toString() {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put(RESPONSE_CODE_FIELD, responseCode.name());  
+        jsonObj.put(RESPONSE_DATA_FIELD, responseData);
+        return jsonObj.toString();
+    }
 }

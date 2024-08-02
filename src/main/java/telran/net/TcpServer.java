ec614e7c668e8b8f9f7fde057b9af3c1788e79f3
IgorMedvedev <psydov81@gmail.com>
2024-08-02 15:05:41 +0300

    hw #42

--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,40 +1,51 @@
 package telran.net;
-import java.net.*;
-import static telran.net.TcpConfigurationProperties.*;
-public class TcpServer implements Runnable{
-	Protocol protocol;
-	int port;
-	boolean running = true;
-	public TcpServer(Protocol protocol, int port) {
-		this.protocol = protocol;
-		this.port = port;
-	}
-	public void shutdown() {
-		running = false;
-	}
-	public void run() {
-		try(ServerSocket serverSocket = new ServerSocket(port)){
-			//TODO using ServerSocket method setSoTimeout 
-			System.out.println("Server is listening on port " + port);
-			serverSocket.setSoTimeout(SOCKET_TIMEOUT);
-			while(running) {
-				try {
-					Socket socket = serverSocket.accept();
 
-					TcpClientServerSession session =
-							new TcpClientServerSession(socket, protocol, this);
-					session.start();
-				} catch (SocketTimeoutException e) {
-					
-				}
-				
-			}
-			
-				
-			
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import static telran.net.TcpConfigurationProperties.*;
+
+public class TcpServer implements Runnable {
+    Protocol protocol;
+    int port;
+    boolean running = true;
+    private final ExecutorService executor;
+
+    public TcpServer(Protocol protocol, int port) {
+        this.protocol = protocol;
+        this.port = port;
+        this.executor = Executors.newFixedThreadPool(getNumberOfThreads());
+    }
+
+    private int getNumberOfThreads() {
+        Runtime runtime = Runtime.getRuntime();
+        return runtime.availableProcessors();
+    }
+
+    public void shutdown() {
+        running = false;
+        executor.shutdown();
+    }
+
+    public void awaitTermination() throws InterruptedException {
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+            serverSocket.setSoTimeout(SOCKET_TIMEOUT);
+            while (running) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    executor.execute(new TcpClientServerSession(socket, protocol, this));
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Server socket timed out waiting for connection: " + e.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
